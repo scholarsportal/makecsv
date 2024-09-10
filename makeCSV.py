f3b31cd51df8e66edbb24a649a630f35253b0c3c
bart kawula <b.kawula@utoronto.ca>
2024-09-10 11:27:23 -0400

    changed special character handling so that it doesn't catch non latin characters

--- makeCSV.py
+++ makeCSV.py
@@ -2,30 +2,34 @@ import os
 import csv
 import re
 import sys
+from pathlib import Path
 
-def find_special_characters(text, log):
+def find_special_characters(text, logfile):
     # Define a regular expression pattern to match special characters
-    special_chars = re.findall(r'[^a-zA-Z0-9\\/]', text)
-    
-    if special_chars:
-        writer = csv.writer(log)
-        msg = "Special characters found [" + ''.join(special_chars) + "] in " + text
-        print("!!! " + msg)
-        writer.writerow([msg])
+    special_chars = re.findall(r"[!@#$%^&*()=+[\]{};':\"<>?\s`]", text)
+    with open(logfile, mode='a', newline='', encoding='utf-8') as log:
+      if special_chars:
+          writer = csv.writer(log)
+          msg = "Special characters replaced [" + ''.join(special_chars) + "] in " + text
+          print("!!! " + msg)
+          writer.writerow([msg])
 
-def find_non_latin(text, log):
+
+def find_non_latin(text, logfile):
     # Regular expression to match any non-Latin character
     non_latin = re.findall(r'[^\u0000-\u007F\u0080-\u00FF]', text)
-    
-    if non_latin:
-        writer = csv.writer(log)
-        msg = "Non Latin characters found [" + ''.join(non_latin) + "] in " + text
-        print("!!! " + msg)
-        writer.writerow([msg])
+    with open(logfile, mode='a', newline='', encoding='utf-8') as log:
+      if non_latin:
+          writer = csv.writer(log)
+          msg = "Non Latin characters found [" + ''.join(non_latin) + "] in " + text
+          print("!!! " + msg)
+          writer.writerow([msg])
 
-def gen_source_files(directory, logfile):
+
+def gen_source_files(directory, bagged, log, makeMetaCSV):
     source_file = os.path.join(directory+'/metadata', "source-metadata.csv")
     with open(source_file, mode='w', newline='', encoding='utf-8') as file1:
+          
        writer1 = csv.writer(file1)
        writer1.writerow(["filename","metadata","type"])
 
@@ -38,24 +42,34 @@ def gen_source_files(directory, logfile):
                     if relative_path.startswith("metadata"):
                       # Skip any hidden files like .DS_Store
                       if relative_path.endswith(".xml"):
-                        find_non_latin(relative_path, logfile)
-                        filename = relative_path.replace("metadata/","")
-                        clean_text = re.sub(r'[^a-zA-Z0-9\\/]', '_', filename)
-                        writer1.writerow(["objects",clean_text])
+                        find_special_characters(relative_path, log)
+                        find_non_latin(relative_path, log)
+                        clean = re.sub(r"[!@#$%^&*()=+[\]{};':\"<>?\s`]", "_", relative_path)
+                        path = Path(clean).parts
+                        parts = list(path)
+                        parts.pop(0)
+                        #filename = os.path.join(*parts)
+                        filename = "/".join(parts)
+                        writer1.writerow(["objects",filename])
 
-def gen_metadata_files(directory, bagged, log_file):
+    print(f"source-metadata.csv created!")
+
+    if (makeMetaCSV == 'y'):
+      gen_metadata_files(directory, bagged, log)
+    else:
+      another(directory)
+
+
+def gen_metadata_files(directory, bagged, log):
     meta_file = os.path.join(directory+'/metadata', "metadata.csv")
     rights_file = os.path.join(directory+'/metadata', "rights.csv")
    
     with open(meta_file, mode='w', newline='', encoding='utf-8') as file1, \
-          open(rights_file, mode='w', newline='', encoding='utf-8') as file2, \
-          open(log_file, mode='w', newline='', encoding='utf-8') as logfile:
+          open(rights_file, mode='w', newline='', encoding='utf-8') as file2:
             
-            #file1.write('\ufeff')
             writer1 = csv.writer(file1)
             writer1.writerow(["filename"])
 
-            #file2.write('\ufeff')
             writer2 = csv.writer(file2)
             writer2.writerow([
                "file",
@@ -78,10 +92,13 @@ def gen_metadata_files(directory, bagged, log_file):
                     full_path = os.path.join(root, dir_name)
                     relative_path = os.path.relpath(full_path, directory)
                     if not relative_path.startswith("metadata"):
-                      find_special_characters(relative_path, logfile)
-                      find_non_latin(relative_path, logfile)
-                      clean_text = re.sub(r'[^a-zA-Z0-9\\/]', '_', relative_path)   
-                      writer1.writerow([clean_text])
+                      clean_text = re.sub(r"[!@#$%^&*()=+[\]{};':\"<>?\s`]", "_", relative_path) 
+                      path = Path(clean_text).parts
+                      parts = list(path)
+                      filename = "/".join(parts)
+                      find_special_characters(filename, log)
+                      find_non_latin(filename, log)
+                      writer1.writerow([filename])
 
                 # Write files
                 for file_name in files:
@@ -89,19 +106,45 @@ def gen_metadata_files(directory, bagged, log_file):
                     relative_path = os.path.relpath(full_path, directory)
                     # Skip the metadata file that's generated
                     if not relative_path.startswith("metadata"):
+                      path = Path(relative_path).parts
+                      parts = list(path)
+                      filename = "/".join(parts)
                       # Skip any hidden files like .DS_Store
                       if not file_name.startswith('.'):
-                        find_non_latin(relative_path, logfile)
-                        if bagged == 'yes': 
-                          writer1.writerow(['data/'+relative_path])
-                          writer2.writerow(['data/'+relative_path])
+                        find_non_latin(filename, log)
+                        if bagged == 'y': 
+                          writer1.writerow(['data/'+filename])
+                          writer2.writerow(['data/'+filename])
                         else:
-                          writer1.writerow([relative_path])
-                          writer2.writerow([relative_path])
+                          writer1.writerow([filename])
+                          writer2.writerow([filename])
 
-    print(f"metadata.csv and rights.csv files have been created successfully.")
+    print(f"metadata.csv created!")
+    print(f"rights.csv created!")
+    another(directory)
 
-def list_directory_contents(directory):
+
+def another(directory):
+
+  if os.path.getsize(directory+'/metadata/log.txt') == 0:
+          os.remove(directory+'/metadata/log.txt')
+
+  while True:
+    try:
+        another = input("Would you like to process another directory? (y/n): ")
+        if another == 'y' or another == 'n':
+          if another == 'y':
+            main()
+          else:
+            print("Okay bye!")
+            sys.exit()
+        else:
+          print("You need to actually type either 'y' for yes or 'n' for no")   
+    except ValueError as e:
+        print(f"An error occurred: {e}")
+
+
+def questions(directory):
     try:
         # Get the list of files and directories
         while True:
@@ -130,13 +173,18 @@ def list_directory_contents(directory):
                 print(f"An error occurred: {e}")
 
         xmlExists = False
-        log_file = os.path.join(directory+'/metadata', "log.txt")
+        bagged = 'n'
 
+        if os.path.exists(directory+'/metadata/log.txt'):
+          os.remove(directory+'/metadata/log.txt')
+
+        logfile = os.path.join(directory+'/metadata', "log.txt")
+        
         while True:
           try:
               if any(fname.endswith('.xml') for fname in os.listdir(directory+'/metadata')):
                 xmlExists = True
-                makeMetaCSV = input("Looks like you're doing an XML Import. Generate metadata.csv and rights.csv? (y/n): ")
+                makeMetaCSV = input("Looks like you're doing an XML Import. Generate source-metadata.csv, metadata.csv and rights.csv? (y/n): ")
                 if  makeMetaCSV == 'y' or  makeMetaCSV == 'n':
                   break
                 else:
@@ -158,30 +206,10 @@ def list_directory_contents(directory):
             except ValueError as e:
               print(f"An error occurred: {e}")
 
-        if (xmlExists):
-          gen_source_files(directory, log_file)
-          if (makeMetaCSV == 'y'):
-            gen_metadata_files(directory, bagged, log_file)
-        else:
-          gen_metadata_files(directory, bagged, log_file)         
-        
-        if os.path.getsize(directory+'/metadata/log.txt') == 0:
-          os.remove(directory+'/metadata/log.txt')
-
-        while True:
-          try:
-              another = input("Would you like to process another directory? (y/n): ")
-              if another == 'y' or another == 'n':
-                if another == 'y':
-                  main()
-                else:
-                  print("Okay bye!")
-                  sys.exit()
-              else:
-                print("You need to actually type either 'y' for yes or 'n' for no")   
-          except ValueError as e:
-              print(f"An error occurred: {e}")
-
+          if (xmlExists):
+            gen_source_files(directory, bagged, logfile, makeMetaCSV)
+          else:
+            gen_metadata_files(directory, bagged, logfile)        
 
     except FileNotFoundError:
         print("The directory path you provided does not exist.")
@@ -196,7 +224,7 @@ def main():
 |__|__|__||___._||__|__||_____||______||_______| \\_____/  
               """)
     directory = input("Please enter the directory path: ").strip()
-    list_directory_contents(directory)
+    questions(directory)
 
 if __name__ == "__main__":
     main()
